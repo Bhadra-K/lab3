56a082fb94bca4c3fcbd4e1b8b4262b785209484
Bhadra <bhadra050127@gmail.com>
2025-10-03 10:20:13 +0530

    passing and destructuring props

---
+++ Props.jsx
@@ -0,0 +1,23 @@
+// // 1.WITHOUT DESTRUCTURING PROPS
+
+// function Welcome(props){
+//    return(
+//     <div>
+//         <h1>Welcome {props.name}!</h1>
+//         <h2>You are {props.age} years old.</h2>
+//     </div>
+//    );
+// }
+// export default Welcome;
+
+
+// // 2.WITH DESTRUCTURING PROPS
+// function Welcome({name,age}){
+//     return(
+//         <div>
+//             <h1>Welcome {name}!</h1>
+//             <h2>You are {age} years old.</h2>
+//         </div>
+//     );
+// }
+// export default Welcome;
